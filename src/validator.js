2847ad36a8c2c638b66bb58e0f88f93d937eae6e
Katie Foster <fosterkatiea@gmail.com>
2017-09-12 17:05:15 -0600

    fix to validator

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,7 @@
 import language from 'language'
 
 // pass in a dict of field / value pairs to the aggregator as testDataObject
-export function validatorAggregator(testDataObject = {}, optDict=null, errorLanguage=null) {
+export function validatorAggregator(testDataObject = {}, errorLanguage=null, optDict=null) {
   let status = { isValid: true, warnings: {}}
 
   const dict = {
@@ -40,7 +40,7 @@ const validate = (prevStatus, testData, validateAs, fieldName, dict, errorLangua
     }
   }
 
-  let error = dict[validateAs](testData, fieldName)
+  let error = dict[validateAs](testData, fieldName, errorLanguage)
   prevStatus.warnings[fieldName] = error
 
   // if (!error) {
@@ -50,48 +50,48 @@ const validate = (prevStatus, testData, validateAs, fieldName, dict, errorLangua
   return { ...prevStatus, isValid: prevStatus.isValid && !error }
 }
 
-function dateError(testData, fieldName) {
+function dateError(testData, fieldName, errorLanguage) {
   // const dateRegex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
   // return dateRegex.test(testData) ? null : errorLanguage ? errorLanguage['invalid-date']
   return new Date(testData) ? null : errorLanguage ? errorLanguage['invalid-date'] : 'Please enter a valid date.'
 }
 
-function emailError(testData, fieldName) {
+function emailError(testData, fieldName, errorLanguage) {
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return emailRegex.test(testData) ? null : errorLanguage ? errorLanguage['invalid-email'] : 'Please enter a valid email.'
 }
 
-function fileError(testData, fieldName) {
+function fileError(testData, fieldName, errorLanguage) {
   return testData.files && testData.files[0] && (testData.files[0].size > 100000)
     ? errorLanguage ? errorLanguage['file-size'] : 'Maximum file size exceeded.'
     : null
 }
 
-function nameError(testData, fieldName) {
+function nameError(testData, fieldName, errorLanguage) {
   return testData.length < 2
     ? errorLanguage ? errorLanguage['min-length'].replace('<FIELD>', fieldName).replace('<LIMIT>', '2') : `${fieldName} must be at least 2 characters.`
     : null
 }
 
-function notEmptyError(testData, fieldName) {
+function notEmptyError(testData, fieldName, errorLanguage) {
   return !testData && (testData !== 0)
     ? errorLanguage ? errorLanguage['empty-field'].replace('<FIELD>', fieldName) : `${fieldName} cannot be empty.`
     : null
 }
 
-function numberError(testData, fieldName) {
+function numberError(testData, fieldName, errorLanguage) {
   return !testData || !(/^[0-9]+$/.test(testData))
     ? errorLanguage ? errorLanguage['invalid-number'] : 'Please enter a valid number.'
     : null
 }
 
-function numberFieldError(testData, fieldName) {
+function numberFieldError(testData, fieldName, errorLanguage) {
   return !testData || !(/^[0-9]+$/.test(testData))
     ? errorLanguage ? errorLanguage['invalid-field'].replace('<FIELD>', fieldName) : `${fieldName} is invalid.`
     : null
 }
 
-function phoneError(testData, fieldName) {
+function phoneError(testData, fieldName, errorLanguage) {
   if (testData.length < 8) {
     return errorLanguage
       ? errorLanguage['min-length'].replace('<FIELD>', language.fieldLabels.phoneNumber).replace('<LIMIT>', '8')
@@ -105,7 +105,7 @@ function phoneError(testData, fieldName) {
   }
 }
 
-function zipError(testData, fieldName) {
+function zipError(testData, fieldName, errorLanguage) {
   const usZipRegex = /^\d{5}(?:[-\s]\d{4})?$/
   return usZipRegex.test(testData)
     ? null
