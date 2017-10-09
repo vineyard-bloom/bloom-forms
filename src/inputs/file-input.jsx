a8c69bbd4bf4717d3cc6dba9f20ddeac8c8a2fe2
Katie Foster <fosterkatiea@gmail.com>
2017-10-09 15:45:32 -0600

    updating styles

--- src/inputs/file-input.jsx
+++ src/inputs/file-input.jsx
@@ -64,7 +64,7 @@ class FileInput extends React.Component {
     return (
       <label htmlFor={ this.props.name } className="input__label input--file" onClick={ this.triggerInput }>
         { label }{ requiredString }
-        <div className='input__placeholder' role='presentation' aria-hidden>
+        <div className='input__placeholder input__placeholder--file' role='presentation' aria-hidden>
           <div className='input--file__text'>
             { this.state.fileText }
           </div>
