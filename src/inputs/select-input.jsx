fbc6510659daa6355a0a992afe6ff00377d120ca
Katie Foster <fosterkatiea@gmail.com>
2018-01-09 15:03:12 -0700

    tweaking error for no matches

--- src/inputs/select-input.jsx
+++ src/inputs/select-input.jsx
@@ -156,7 +156,7 @@ class SelectInput extends React.Component {
 
     this.setState({
       hasUsedPresentationElements: true,
-      noMatches: !!sortValue && !!sortedOpts.length,
+      noMatches: (this.props.value || !!sortValue) && !!sortedOpts.length,
       showList: true,
       sortBy: sortValue,
       sortedOpts: sortedOpts
@@ -168,6 +168,7 @@ class SelectInput extends React.Component {
     this.setState({
       hasUsedPresentationElements: true,
       focusedOpt: null,
+      noMatches: !this.state.showList ? noMatches : false,
       showList: !this.state.showList
     });
   };
